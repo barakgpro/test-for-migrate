72514f3cee2c1cb5f95c3329a8b99b8516621e45
barakg <barak.glick@proteantecs.com>
2022-01-27 17:26:35 +0200

    tests2

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,8 @@
-print('hello world')`
+import jenkins
+
+server = jenkins.Jenkins('http://localhost:9080', username='admin', password='admin')
+server.create_job('test_from_python', jenkins.EMPTY_CONFIG_XML)
+user = server.get_whoami()
+version = server.get_version()
+print('Hello %s from Jenkins %s' % (user['fullName'], version))
+x=0
